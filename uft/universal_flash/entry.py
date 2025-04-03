14a30f0ab48eda6124812140433e96ac33bf8af8
ChunAn Wu <an.wu@canonical.com>
2025-04-03 14:57:04 +0800

    for support custom arguments
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- uft/universal_flash/entry.py
+++ uft/universal_flash/entry.py
@@ -2,10 +2,16 @@ import subprocess
 import sys
 
 def main():
+    flashing = None
     result = subprocess.run("lsusb", capture_output=True, text=True, shell=True)
     if "NXP Semiconductors SE Blank 865" in result.stdout:
         print("NXP uuu device is found")
-        flashing = subprocess.Popen("uuu uc.lst", text=True, shell=True)
+        if len(sys.argv) > 1:
+            cmd = "uuu " + (' '.join(sys.argv[1:]))
+        else:
+            cmd = "uuu uc.lst"
+        flashing = subprocess.Popen(cmd, text=True, shell=True)
 
     # Wait for the process to finish
-    flashing.wait()
+    if flashing is not None:
+        flashing.wait()
