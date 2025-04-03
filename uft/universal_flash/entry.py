4f87d43284c86bdf3111d616e63fe8654737f272
ChunAn Wu <an.wu@canonical.com>
2025-04-03 22:47:01 +0800

    Add waiting for target device be ready
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- uft/universal_flash/entry.py
+++ uft/universal_flash/entry.py
@@ -1,16 +1,21 @@
 import subprocess
 import sys
+import time
 
 def main():
     flashing = None
-    result = subprocess.run("lsusb", capture_output=True, text=True, shell=True)
-    if "NXP Semiconductors SE Blank 865" in result.stdout:
-        print("NXP uuu device is found")
-        if len(sys.argv) > 1:
-            cmd = "uuu " + (' '.join(sys.argv[1:]))
-        else:
-            cmd = "uuu uc.lst"
-        flashing = subprocess.Popen(cmd, text=True, shell=True)
+    print("Waiting for target device....")
+    while True:
+        result = subprocess.run("lsusb", capture_output=True, text=True, shell=True)
+        if "NXP Semiconductors SE Blank 865" in result.stdout:
+            print("NXP uuu device is found")
+            if len(sys.argv) > 1:
+                cmd = "uuu " + (' '.join(sys.argv[1:]))
+            else:
+                cmd = "uuu uc.lst"
+            flashing = subprocess.Popen(cmd, text=True, shell=True)
+            break
+        time.sleep(3)
 
     # Wait for the process to finish
     if flashing is not None:
