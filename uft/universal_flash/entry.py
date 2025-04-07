f562b5616c90a927ba2972cef7098f612cac0c05
ChunAn Wu <an.wu@canonical.com>
2025-04-07 16:34:42 +0800

    It could be multiple devices share same method Using list to do the match
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- uft/universal_flash/entry.py
+++ uft/universal_flash/entry.py
@@ -2,12 +2,18 @@ import subprocess
 import sys
 import time
 
+uuu = [
+    "NXP Semiconductors SE Blank 865",
+    "Freescale Semiconductor, Inc. SE Blank PELE",
+    "Freescale Semiconductor, Inc. i.MX 6Dual/6Quad SystemOnChip in RecoveryMode"
+]
+
 def main():
     flashing = None
     print("Waiting for target device....")
     while True:
         result = subprocess.run("lsusb", capture_output=True, text=True, shell=True)
-        if "NXP Semiconductors SE Blank 865" in result.stdout:
+        if any(device in result.stdout for device in uuu):
             print("NXP uuu device is found")
             if len(sys.argv) > 1:
                 cmd = "uuu " + (' '.join(sys.argv[1:]))
